79003ccbb93707f97ce140d2722c0bff2d279379
Jose Nuñez <josenumal@gmail.com>
2023-06-14 18:31:50 +0200

    insert Products

---
+++ src/main/resources/db/migration/V1.2__insert_products.sql
@@ -0,0 +1,10 @@
+INSERT INTO
+    coffee_shop_dev.products (product_name, price)
+VALUES
+    ('Latte', 5.3),
+    ('Espresso', 4),
+    ('Sandwich', 10.10),
+    ('Milk', 1),
+    ('Cake Slice', 9),
+    ('Capuccino',8),
+    ('Tea',6.1);
