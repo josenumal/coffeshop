5ed299b007ba7368221099d1845a510971037d0b
Jose Nuñez <josenumal@gmail.com>
2023-06-22 16:43:16 +0200

    fix mapper

--- src/main/java/com/inatlas/coffeeshop/mappers/ProductMapper.java
+++ src/main/java/com/inatlas/coffeeshop/mappers/ProductMapper.java
@@ -3,12 +3,14 @@ package com.inatlas.coffeeshop.mappers;
 import com.inatlas.coffeeshop.dtos.ProductDto;
 import com.inatlas.coffeeshop.entities.Product;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 import java.util.List;
 
 @Mapper(componentModel = "Spring")
 public interface ProductMapper {
 
+    @Mapping(target = "productName", source = "productName")
     ProductDto productToProductDto(Product product);
 
     List<ProductDto> productListToProductDtoList(List<Product> productList);
